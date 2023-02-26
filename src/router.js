852c153e0c035c95114eb7f8d333fb1d26eb985a
HectorFranc0u <halejandrofb@gmail.com>
2023-02-25 21:01:05 -0600

    first commit

---
+++ src/router.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+    {
+      path: '/Introduccion',
+      name: 'Introduccion',
+      component: () =>  import('./components/Introduccion.vue'),
+    },
+    {
+      path: '/',
+      name: 'Portada',
+      component: () => import('./components/Portada.vue')
+    },
+    {
+      path: '/Apuntes',
+      name: 'Apuntes',
+      component: () => import('./components/Contenidos/Apuntes.vue')
+    },
+    {
+      path: '/Investigaciones',
+      name: 'Investigaciones',
+      component: () => import('./components/Contenidos/Investigaciones.vue')
+      },
+    {
+    path: '/SdC',
+    name: 'SdC',
+    component: () => import('./components/Contenidos/SdC.vue')
+    },
+    {
+    path: '/Tareas',
+    name: 'Tareas',
+    component: () => import('./components/Contenidos/Tareas.vue')
+    }
+    
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
